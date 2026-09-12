b8eaafb3f39c7b754dfd54691b2b5b4783bc4842
Sahidhya Kulasekar <36248406+sahidhya@users.noreply.github.com>
2026-09-12 09:55:37 -0700

    Generated model files

--- assignment/models/Subreddit.js
+++ assignment/models/Subreddit.js
@@ -1,25 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const SubredditSchema = new mongoose.Schema({
-    name: { 
-        type: String, 
-        required: true, 
-        unique: true 
-    },
-    description: { 
-        type: String 
-    },
-    author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        required: true
-    }
+const subredditSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  description: { type: String },
+  author: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
+  createdAt: { type: Date, required: true },
 });
 
-const Subreddit = mongoose.model('Subreddit', SubredditSchema);
+const Subreddit = mongoose.model("Subreddit", subredditSchema);
 
 export default Subreddit;
--- assignment/models/Thread.js
+++ assignment/models/Thread.js
@@ -1,42 +1,16 @@
 import mongoose from "mongoose";
 
-const ThreadSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  subreddit: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Subreddit",
-    required: true,
-  },
-  upvotes: {
-    type: Number,
-    default: 0,
-  },
-  downvotes: {
-    type: Number,
-    default: 0,
-  },
-  voteCount: {
-    type: Number,
-    default: 0,
-  },
-  createdAt: {
-    type: Date,
-    required: true,
-  },
+const threadSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
+  subreddit: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Subreddit" },
+  upvotes: { type: Number, default: 0 },
+  downvotes: { type: Number, default: 0 },
+  voteCount: { type: Number, default: 0 },
+  createdAt: { type: Date, required: true },
 });
 
-const Thread = mongoose.model("Thread", ThreadSchema);
+const Thread = mongoose.model("Thread", threadSchema);
 
 export default Thread;
